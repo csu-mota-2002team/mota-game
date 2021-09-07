c6e5968068b6f43c851008280796ae00874f4df7
3110900264 <86938768+3110900264@users.noreply.github.com>
2021-09-07 14:40:22 +0800

    Update MonsterArr.java

--- MonsterArr.java
+++ MonsterArr.java
@@ -3,6 +3,6 @@ package com.nll.oop1;
 public class MonsterArr {
     static Monster m1 = new Monster("史莱姆",100,20,20,2);
     static Monster m2 = new Monster("骷髅兵",150,30,30,3);
-    static Monster m4 = new Monster("吸血鬼",300,40,40,4);
-    public static Monster[] ms = new Monster[]{m1,m2,m4};
+    static Monster m4 = new Monster("骑士",300,40,40,4);
+    public static Monster[] ms = new Monster[]{m1,m2,m3};
 }
